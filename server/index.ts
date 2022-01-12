194331e76d8053bdbe0d2dc67c45f37fca03b346
Zou Minowa <zouminowa@gmail.com>
2022-01-12 16:26:55 +0000

    feat(server): add boilerplate router

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,16 @@
 import dotenv from 'dotenv';
 import express from 'express';
 
+import router from './custom_middleware/router';
+
 // add environment variables
 dotenv.config();
 
 // create server
 const app = express();
+
+// middleware
+app.use(router);
 
 // start server
 const PORT = process.env.SERVER_PORT || 3000;
