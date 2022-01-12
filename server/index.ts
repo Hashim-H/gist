1a2e424c1ab41e6f0c1f837ad6c77a45956451a0
Zou Minowa <zouminowa@gmail.com>
2022-01-12 16:22:15 +0000

    refactor(server): refactor server to use typescript

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,12 @@
-require('dotenv').config();
-const express = require('express');
+import dotenv from 'dotenv';
+import express from 'express';
 
+// add environment variables
+dotenv.config();
+
+// create server
 const app = express();
 
+// start server
 const PORT = process.env.SERVER_PORT || 3000;
 app.listen(PORT, () => console.log(`server: http://localhost:${PORT}`));
