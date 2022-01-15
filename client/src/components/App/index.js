bc302bbd2a6d77555a5a6aecc17a850d087427ec
Zou Minowa <zouminowa@gmail.com>
2022-01-15 16:15:18 +0000

    refactor(client): cleaned up App component

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,26 +1,25 @@
-// style
-import './App.css';
+// styles
+import styles from './App.module.css';
 
 // libraries
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// api
+// store
 import { getLists } from '../../redux/slices/lists';
 
 // components
 import Header from '../structural/Header';
 import UserLists from '../view/UserLists';
 import GameList from '../view/GameList';
+import ListEditor from '../view/ListEditor';
 
 export default function App() {
 
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getLists());
-  }, [dispatch]);
+  useEffect(() => dispatch(getLists()), [dispatch]);
 
   return (
     <div className="app">
@@ -29,6 +28,8 @@ export default function App() {
         <Routes>
           <Route path="/" exact element={<UserLists />} />
           <Route path="/gamelist/:id" element={<GameList />} />
+          <Route path="/listeditor/" element={<ListEditor />} />
+          <Route path="/listeditor/:id" element={<ListEditor />} />
         </Routes>
       </Router>
     </div>
