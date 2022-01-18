ed62a55973b2037e80bd308f6d8e3c8336411442
Zou Minowa <zouminowa@gmail.com>
2022-01-18 18:04:49 +0000

    fix(client): fixed list re-render bug

--- client/src/components/views/ListEditorView/GamePicker/OwnedGameList/index.js
+++ client/src/components/views/ListEditorView/GamePicker/OwnedGameList/index.js
@@ -10,46 +10,45 @@ import ListItem from '../../../../containers/lists/ListItemContainer';
 import GameImage from '../../../../features/GameImage';
 
 export default function OwnedGameList({ ownedGames, listGames, onAddGame }) {
-
-  // render helper functions
-  const Container = ({ children }) => {
-    return (
-      <div className={styles.containerOuter}>
-        <div className={styles.containerInner}>
-          { children }
-        </div>
-      </div>
-    );
-  };
-
-  const ListItems = () => {
-    return ownedGames.map(game => {
-      const added = listGames.find(listGame => listGame.appid === game.appid);
-
-      return (
-        <div
-          key={game.appid}
-          className={added ? styles.notAllowed : styles.allowed}
-          onClick={added ? null : () => onAddGame(game)}>
-          <ListItem>
-            <div className={styles.flexContainer}>
-              <GameImage appid={game.appid} hash={game.img_logo_url} />
-              <h4 className={styles.gameName}>{game.name}</h4>
-              {added ? <IoCheckmark className={styles.added} /> : null}
-            </div>
-          </ListItem>
-        </div>
-      );
-    });
-  };
-
   // render
   return (
     <Container>
-      {/* <List> */}
-        <ListItems />
-      {/* </List> */}
+      <List>
+        <ListItems ownedGames={ownedGames} listGames={listGames} onAddGame={onAddGame} />
+      </List>
     </Container>
   );
 }
+
+// render helper functions
+function Container ({ children }) {
+  return (
+    <div className={styles.containerOuter}>
+      <div className={styles.containerInner}>
+        { children }
+      </div>
+    </div>
+  );
+};
+
+function ListItems({ ownedGames, listGames, onAddGame }) {
+  return ownedGames.map(game => {
+    const added = listGames.find(listGame => listGame.appid === game.appid);
+
+    return (
+      <div
+        key={game.appid}
+        className={added ? styles.notAllowed : styles.allowed}
+        onClick={added ? null : () => onAddGame(game)}>
+        <ListItem>
+          <div className={styles.flexContainer}>
+            <GameImage appid={game.appid} hash={game.img_logo_url} />
+            <h4 className={styles.gameName}>{game.name}</h4>
+            {added ? <IoCheckmark className={styles.added} /> : null}
+          </div>
+        </ListItem>
+      </div>
+    );
+  });
+};
 
